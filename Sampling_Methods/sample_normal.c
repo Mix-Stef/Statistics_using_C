a2192472e38002decea94bccf2fa774378ebdfe3
Michael Stefanis <48247084+Mix-Stef@users.noreply.github.com>
2023-07-26 19:26:44 +0300

    Update sample_normal.c

--- Sampling_Methods/sample_normal.c
+++ Sampling_Methods/sample_normal.c
@@ -5,30 +5,63 @@
 
 //Samples from standard normal distribution using the Box-Muller transform.
 //Write the samples to a csv file with two columns. Each row contains two
-//independent gaussian random numbers.
+//independent gaussian random numbers. Using the Box-Muller transform we can
+//generate a synthetic dataset for linear regression tests of the form 
+//(x, y) = (x, a * x + b + Gaussian_Noise(mu, var))
 
 int main(int argc, char *argv[]){
 
+
     double pi = 3.1415;
     srand(time(NULL)); //RNG
-    int N = atoi(argv[1]); //Number of points
+    unsigned int N = atoi(argv[1]); //Number of points
+    double mu = atof(argv[2]); //The mean of the gaussian
+    double sigma = atof(argv[3]); //The standard deviation
+
+    //Generate pair of independent gaussian random variables
+    
+    // FILE *fp;
+    // fp = fopen("random_normal_new.csv", "w");
+    // if (fp == NULL){
+    //     printf("Error with file pointer!");
+    //     exit(0);
+    // }
+    // fprintf(fp, "x,y\n");
+    // for (int i=0;i<N;i++){
+    //     double u1 = (rand() % 1000) / (double)1000;
+    //     double u2 = (rand() % 1000) / (double)1000;
+    //     double z1 = sigma * sqrt(-2.0 * log(u1)) * cos(2.0 * pi * u2) + mu;
+    //     double z2 = sigma * sqrt(-2.0 * log(u1)) * sin(2.0 * pi * u2) + mu;
+    //     printf("%f %f \n", z1, z2);
+    //     if (isinf(z1) == 0 && isinf(z2) == 0){
+    //         fprintf(fp, "%f,%f\n", z1, z2);
+    //     }
+    // }
+    // fclose(fp);
+
+
+    //Generate dataset for linear regression y = a * x + b + gaussian_error(mu=0, sigma=2)
+    double step = 0.01;
     FILE *fp;
-    fp = fopen("random_normal.csv", "w");
+    fp = fopen("linear_regression.csv", "w");
     if (fp == NULL){
         printf("Error with file pointer!");
         exit(0);
     }
     fprintf(fp, "x,y\n");
     for (int i=0;i<N;i++){
-        double u1 = (rand() % 1000) / (double)1000; //Random uniform in (0,1)
-        double u2 = (rand() % 1000) / (double)1000; //Random uniform in (0,1)
-        double z1 = sqrt(-2.0 * log(u1)) * cos(2.0 * pi * u2); //Box-Muller Transform
-        double z2 = sqrt(-2.0 * log(u1)) * sin(2.0 * pi * u2); //Box-Muller Transform
+        double x_value = 0 + i * step;
+        double u1 = (rand() % 1000) / (double)1000;
+        double u2 = (rand() % 1000) / (double)1000;
+        double z1 = sigma * sqrt(-2.0 * log(u1)) * cos(2.0 * pi * u2) + mu;
+        double y_value = 2 * x_value + 3 + z1;
         // printf("%f %f \n", z1, z2);
-        if (isinf(z1) == 0 && isinf(z2) == 0){ //May produce infinities due to numerical instability so we discard them
-            fprintf(fp, "%f,%f\n", z1, z2);
+        if (isinf(z1) == 0){
+            fprintf(fp, "%f,%f\n", x_value, y_value);
         }
     }
     fclose(fp);
+
+
     return 0;
 }
